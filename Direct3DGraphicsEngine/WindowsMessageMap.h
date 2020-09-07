479d8cc8e0b34abb3637c4a129a3edce5370810f
KylePearce-SoftwareDeveloper <kylepearcesoftwaredeveloper@gmail.com>
2020-09-07 15:23:07 +0100

    #0 - Continued From Other Project - Fixed Window Icon Not Working.

---
+++ Direct3DGraphicsEngine/WindowsMessageMap.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <unordered_map>
+#include "WinDef.h"
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+class WindowsMessageMap
+{
+public:
+	WindowsMessageMap() noexcept;
+	std::string operator()(DWORD msg, LPARAM lp, WPARAM wp) const noexcept;
+private:
+	std::unordered_map<DWORD, std::string> map;
+};
+
