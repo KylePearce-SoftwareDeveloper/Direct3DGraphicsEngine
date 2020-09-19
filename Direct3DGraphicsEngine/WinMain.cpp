00131777bb1845a0750a081423769fd4cd919a2e
KylePearce-SoftwareDeveloper <kylepearcesoftwaredeveloper@gmail.com>
2020-09-19 15:27:41 +0100

    #3 - App Class / PeekMessage

--- Direct3DGraphicsEngine/WinMain.cpp
+++ Direct3DGraphicsEngine/WinMain.cpp
@@ -1,5 +1,5 @@
-#include "Window.h"
-#include <sstream>
+#include "App.h"
+
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -9,51 +9,7 @@ int CALLBACK WinMain(
 {
 	try
 	{
-		Window wnd(800, 300, "Donkey Fart Box");
-
-		MSG msg;
-		BOOL gResult;
-		while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
-		{
-			// TranslateMessage will post auxilliary WM_CHAR messages from key msgs
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-
-			// test code
-			static int i = 0;
-			while (!wnd.mouse.IsEmpty())
-			{
-				const auto e = wnd.mouse.Read();
-				switch (e.GetType())
-				{
-				case Mouse::Event::Type::WheelUp:
-					i++;
-					{
-						std::ostringstream oss;
-						oss << "Up: " << i;
-						wnd.SetTitle(oss.str());
-					}
-					break;
-				case Mouse::Event::Type::WheelDown:
-					i--;
-					{
-						std::ostringstream oss;
-						oss << "Down: " << i;
-						wnd.SetTitle(oss.str());
-					}
-					break;
-				}
-			}
-		}
-
-		// check if GetMessage call itself borked
-		if (gResult == -1)
-		{
-			throw CHWND_LAST_EXCEPT();
-		}
-
-		// wParam here is the value passed to PostQuitMessage
-		return msg.wParam;
+		return App{}.Go();
 	}
 	catch (const Exception& e)
 	{
